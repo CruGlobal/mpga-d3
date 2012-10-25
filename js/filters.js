8ea7065b987941bf4fbd7b8a9693135cdd30dd1d
Robby Ronk <robby.ronk@gmail.org>
2012-10-25 09:50:39 -0400

    DRYed up filters and added moment

--- js/filters.js
+++ js/filters.js
@@ -16,46 +16,34 @@
         return _.chain(partners).
           pluck('twelveMonthTotalAmount').
           reduce(function (a, b) {
-            return a + b;
+            if(_.isUndefined(b))
+              return parseFloat(a);
+            else
+              return parseFloat(a) + parseFloat(b);
           }, 0).
           value();
       };
     }).
-    filter('monthlyAmount',function () {
+    filter('monthlyAmount', ['$filter', function (filter) {
       return function (partners) {
-        return _.chain(partners).
-          pluck('twelveMonthTotalAmount').
-          reduce(function (a, b) {
-            return a + b;
-          }, 0).
-          value() / 12;
+        return filter('amount')(partners) / 12;
       };
-    }).
-    filter('monthlyAmountPerPartner',function () {
+    }]).
+    filter('monthlyAmountPerPartner', ['$filter', function (filter) {
       return function (partners) {
-        return _.chain(partners).
-          pluck('twelveMonthTotalAmount').
-          reduce(function (a, b) {
-            return a + b;
-          }, 0).
-          value() / 12 / _.size(partners);
+        return filter('monthlyAmount')(partners) / _.size(partners);
       };
-    }).
+    }]).
     filter('size',function () {
       return function (arr) {
         return _.size(arr);
       };
     }).
-    filter('amountPercentage',function () {
+    filter('amountPercentage', ['$filter', function (filter) {
       return function (partners, total) {
-        return _.chain(partners).
-          pluck('twelveMonthTotalAmount').
-          reduce(function (a, b) {
-            return a + b;
-          }, 0).
-          value() / total * 100;
+        return filter('amount')(partners) / total * 100;
       };
-    }).
+    }]).
     filter('countPercentage',function () {
       return function (partners, total) {
         return _.size(partners) / total * 100;
@@ -115,6 +103,11 @@
               return a + b;
             }, 0).
             value();
+      };
+    }).
+    filter('moment', function () {
+      return function (date, format) {
+        return moment(date).format(format);
       };
     });
 })();
