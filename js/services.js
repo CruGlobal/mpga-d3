da19fc326edf0434c9ab32303e537fcb8fd3592a
Matthew Drees <matt.drees@cru.com>
2012-11-02 22:27:04 -0600

    add error handling for 401 responses (when users has been logged out of CAS)

--- js/services.js
+++ js/services.js
@@ -11,14 +11,38 @@
 //    var schemeHostAndPort = 'http://localhost:8680';
       var schemeHostAndPort = 'http://hart-a321.net.ccci.org:9980';
       var corsUrl = schemeHostAndPort + '/wsapi/easyXDM/cors/';
+      function log(something)
+      {
+        if ( console.log ) {
+          if (something != undefined)
+            console.log(something);
+        }
+      }
 
-      var xhr = new easyXDM.Rpc({
+
+      var schemeHostAndPort = 'http://localhost:8680';
+//      var schemeHostAndPort = 'http://hart-a321.net.ccci.org:9980';
+//      var schemeHostAndPort = 'https://wsapistaging.ccci.org';
+
+      var xhr;
+
+      reloadXhr();
+
+      /**
+       * as a side effect, this re-requests (if necessary) a new ticket from CAS, since the iframe url
+       * is protected by the cas filter.
+       */
+      function reloadXhr()
+      {
+        var corsUrl = schemeHostAndPort + '/wsapi/easyXDM/cors/';
+        xhr = new easyXDM.Rpc({
           remote: corsUrl
-      }, {
+        }, {
           remote: {
-              request: {} // request is exposed by /cors/
+            request: {} // request is exposed by /cors/
           }
-      });
+        });
+      }
 
       var cache = $cacheFactory('EasyXdm-Cache');
 
@@ -28,33 +52,87 @@
 
               var url = schemeHostAndPort + pathAndQueryString;
 
+              function handleSuccessfulResponse(response) {
+                var data = response.data;
+                var contentType = response.headers["Content-Type"];
+                var resolution;
+                if (contentType == 'plain/text')
+                  resolution = data;
+                else {
+                  resolution = angular.fromJson(data);
+                }
+                deferred.resolve(resolution);
+                cache.put(pathAndQueryString, resolution);
+              }
+
+              function handleFailedRequest(response) {
+                log("request to " + url + " failed.");
+                var message;
+                if (response)
+                {
+                  log(response);
+                  message = "Request failed; response code is " + response.status + " when calling " + url;
+                }
+                else
+                {
+                  message = "Request failed; no response given";
+                }
+                deferred.reject(message);
+              }
+
+              function sendRequestToUrl(retryOnUnauthorized) {
+                xhr.request({
+                  url:url,
+                  method:"GET"
+                }, function (response) {
+                  if (response.status == 200) {
+                    handleSuccessfulResponse(response);
+                  }
+                  else {
+                    //TODO: what to do here?
+                    handleFailedRequest(response);
+                  }
+                }, function (errorPayload){
+                  var message = errorPayload.message;
+                  var response = errorPayload.data;
+                  if (retryOnUnauthorized && response != undefined && response.status == 401) {
+                    handleNotAuthorizedResponse();
+                  }
+                  else
+                  {
+                    handleFailedRequest(response);
+                  }
+                });
+              }
+
+              function handleNotAuthorizedResponse() {
+                reloadXhr();
+
+                var loggedInToCas = xhr.request({
+                  url:"/wsapi/rest/authentication/loggedInToCas",
+                  method:"GET"
+                }, function (response) {
+                  if (response.data === "true") {
+                    var retryOnUnauthorized = false;
+                    sendRequestToUrl(retryOnUnauthorized);
+                  }
+                  else {
+                    alert("It appears you are not logged in to Relay.  Attempting to reload the page...");
+                    deferred.reject("User is not logged in");
+                    window.top.location.reload();
+                  }
+                }, function (errorPayload){
+                  var message = errorPayload.message;
+                  var response = errorPayload.data;
+                  log("unable to check if logged in to cas: " + message);
+                  log(response);
+                  deferred.reject("Unable to check if logged in to cas; response code " + response.status);
+                });
+              }
+
               if (cache.get(pathAndQueryString) == undefined)
               {
-                xhr.request({
-                    url: url,
-                    method: "GET"
-                }, function(response) {
-                    scope.$apply(function() {
-                        if (response.status == 200)
-                        {
-                            var data = response.data;
-                            var contentType = response.headers["Content-Type"];
-                            var resolution;
-                            if (contentType == 'plain/text')
-                                resolution = data;
-                            else
-                            {
-                                resolution = angular.fromJson(data);
-                            }
-                            deferred.resolve(resolution);
-                            cache.put(pathAndQueryString, resolution);
-                        }
-                        else
-                        {
-                            deferred.reject("Invocation Errors Occurred and the status is " + response.status + " when calling " + url);
-                        }
-                    });
-                });
+                sendRequestToUrl(true);
               }
               else
               {
@@ -62,7 +140,7 @@
                 deferred.resolve(resolution);
               }
 
-                return deferred.promise;
+              return deferred.promise;
             }
         };
     }]);
