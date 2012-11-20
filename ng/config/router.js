69d934deab5650a0979afec5abb8ab0d75f8507b
Robby Ronk <robby.ronk@gmail.org>
2012-11-20 15:17:45 -0500

    Move the templates into a folder named templates
    Templates are not called 'partials' in Angular

--- ng/config/router.js
+++ ng/config/router.js
@@ -4,12 +4,12 @@
   angular.module('mpga', ['mpgaControllers', 'mpgaFilters', 'mpgaServices', 'mpgaDirectives'])
     .config(['$routeProvider', function ($routeProvider) {
     $routeProvider
-      .when('/current-partners', { templateUrl:'partials/current-partners.html', controller:'CurrentPartnersController'})
-      .when('/lost-partners', { templateUrl:'partials/lost-partners.html', controller:'LostPartnersController'})
-      .when('/statistical-analysis', { templateUrl:'partials/statistical-analysis.html', controller:'StatisticalAnalysisController'})
-      .when('/giving-range', { templateUrl:'partials/giving-range.html', controller:'GivingRangeController'})
-      .when('/giving-frequency', { templateUrl:'partials/giving-frequency.html', controller:'GivingFrequencyController'})
-      .when('/income-expenses', { templateUrl:'partials/income-expenses.html', controller:'IncomeExpensesController'})
+      .when('/current-partners', { templateUrl:'ng/templates/current-partners.html', controller:'CurrentPartnersController'})
+      .when('/lost-partners', { templateUrl:'ng/templates/lost-partners.html', controller:'LostPartnersController'})
+      .when('/statistical-analysis', { templateUrl:'ng/templates/statistical-analysis.html', controller:'StatisticalAnalysisController'})
+      .when('/giving-range', { templateUrl:'ng/templates/giving-range.html', controller:'GivingRangeController'})
+      .when('/giving-frequency', { templateUrl:'ng/templates/giving-frequency.html', controller:'GivingFrequencyController'})
+      .when('/income-expenses', { templateUrl:'ng/templates/income-expenses.html', controller:'IncomeExpensesController'})
       .otherwise({redirectTo:'/current-partners'});
   }])
 })();
