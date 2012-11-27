d6331fc2b75fd13035b54b0879e23d4c2fa3e09b
Robby Ronk <robby.ronk@gmail.org>
2012-11-27 09:53:35 -0500

    Add new directive for expenses pie chart on Income/Expenses page

---
+++ ng-app-mpga/modules/income-expenses/directives/expenses-pie-chart.js
@@ -0,0 +1,43 @@
+'use strict';
+
+(function () {
+  angular.module('mpga.income-expenses')
+    .directive('expensesPieChart',function () {
+      return {
+        scope:{
+          input:'=',
+          prefixUnits:'=',
+          postfixUnits:'='
+        },
+        link:function (scope, element, attrs) {
+          scope.$watch('input', function(newInput, oldInput) {
+            if(!newInput) {
+              return;
+            }
+
+            nv.addGraph(function() {
+              var chart = nv.models.pieChart()
+                .x(function(d) { return d.label })
+                .y(function(d) { return d.value })
+                .tooltips(true)
+                .tooltipContent(function(key, y, e, graph) {
+                  return '<div class="pie-tooltip">' +
+                    '<span class="pie-value"> ' + key + ': $' + y + '</span> ' +
+                    '</div>';
+                })
+                .showLabels(false);
+
+              chart.valueFormat(d3.format(',.0f'));
+
+              d3.select(element[0])
+                .datum(scope.input)
+                .transition().duration(1200)
+                .call(chart);
+
+              return chart;
+            });
+          });
+        }
+      }
+    })
+})();
