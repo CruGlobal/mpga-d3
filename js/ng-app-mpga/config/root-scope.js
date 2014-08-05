3829f8fc4d1a9815fd4e922e8a105c2238fd7c4b
adammeyer <adam.meyer@cru.org>
2014-08-05 10:03:34 -0400

    Set oneYearAgo date to beginning of month

--- js/ng-app-mpga/config/root-scope.js
+++ js/ng-app-mpga/config/root-scope.js
@@ -6,6 +6,8 @@
     rootScope.isLostPartner = function (partner) {
       var oneYearAgo = new Date();
       oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
+      oneYearAgo.setDate(1);
+      oneYearAgo.setHours(0, 0, 0, 0);
       var partnerLastTransactionDate = new Date(partner.lastTransactionDate);
 
       return partnerLastTransactionDate.getTime() <= oneYearAgo.getTime();
